1c5beb308eae8f36b4b07e45932e2745c3e3743e
muhammadh <muhammad.hamza@xflowresearch.com>
2022-04-14 11:33:11 +0500

    update run script

--- run.sh
+++ run.sh
@@ -3,15 +3,15 @@ LOG_PARAMS="--log.all_level=debug"
 PORT_ARGS="tx_port=tcp://*:2100,rx_port=tcp://localhost:2101"
 ZMQ_ARGS="--rf.device_name=zmq --rf.device_args=\"${PORT_ARGS},id=ue,base_srate=23.04e6\" --gw.netns=ue1"
 ## Create netns for UE
-ip netns list | grep "ue1" > /dev/null
+# ip netns list | grep "ue1" > /dev/null
 
-if [ $? -eq 1 ]; then
-    echo creating netspace ue1...
-    ip netns add ue1
-    if [ $? -ne 0 ]; then
-    echo failed to create netns ue1
-    exit 1
-    fi
-fi
+# if [ $? -eq 1 ]; then
+#     echo creating netspace ue1...
+#     ip netns add ue1
+#     if [ $? -ne 0 ]; then
+#     echo failed to create netns ue1
+#     exit 1
+#     fi
+# fi
 
 srsue ue.conf ${LOG_PARAMS} ${ZMQ_ARGS} --rat.eutra.dl_earfcn=2850 "$@"
